fb7ba0977cf60ac48fafdb042a72b9c0d4e00e89
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-05-20 22:04:57 -0500

    :v: passing username as params between components (welcome to chat)

---
+++ src/components/Chat.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="chat container">
+        <h2>Chat - {{username}}</h2>
+    </div>    
+</template>
+<script>
+    export default {
+        name: 'chat',
+        props: ['name'],
+        data(){
+            return {
+
+            }
+        },
+        computed: {
+            username(){
+                return this.name
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
