e484102cfd6375569dbd5825671c28809be2fe56
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-05-20 22:22:16 -0500

    :v: NewMessage Component
    component created to handle new messages on the chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="chat container">
-        <h2>Chat - {{username}}</h2>
+        <h2 class="center teal-text">Chat - {{username}}</h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">Message</span>
+                        <span class="grey-text time">Time</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <NewMessage :name="name"></NewMessage>
+            </div>
+        </div>
     </div>    
 </template>
 <script>
+    import NewMessage from '@/components/NewMessage'
     export default {
         name: 'chat',
         props: ['name'],
+        components: {
+            NewMessage
+        },
         data(){
             return {
 
@@ -20,5 +38,15 @@
     }
 </script>
 <style>
-
+    .chat h2{
+        font-size: 2.6em;
+        margin-bottom: 40px;
+    }
+    .chat span{
+        font-size: 1.4em;
+    }
+    .chat .time{
+        display: block;
+        font-size: 1.2em;
+    }
 </style>
---
+++ src/components/NewMessage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">New message (enter to add)</label>
+            <input type="text" v-model="newMessage" name="new-message" id="new-message">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewMessage',
+        props: ['name'],
+        data(){
+            return {
+                newMessage: null
+            }
+        },
+        methods: {
+            addMessage(){
+                if (this.newMessage) {
+                    console.log(this.name, this.newMessage, Date.now())
+                }
+                else {
+
+                }
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
